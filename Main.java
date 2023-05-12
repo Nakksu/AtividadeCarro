9bf928f2b7923774b906f5fadb96684fe16b378b
Emily Carolina <128648356+Huoyiin@users.noreply.github.com>
2023-05-12 20:52:44 -0300

    Main.java

--- Main.java
+++ Main.java
@@ -2,14 +2,22 @@ public class Main {
     public static void main(String[] args) {
         Carro carro = new Carro();
 
-        carro.setCor("Amarelo");
+        carro.setCombustivel(20);
+        carro.setMarca("Fiat");
+        carro.setPeso(90);
+        carro.setAnoFabricacao(2015);
+        carro.setCor("Azul");
         carro.setPlaca("MDR995");
-        carro.setTipo("Fiat");
+        carro.setTipo("Esportivo");
         carro.setNumPortas(4);
 
-        System.out.println(carro.getCor());
-        System.out.println(carro.getPlaca());
-        System.out.println(carro.getTipo());
-        System.out.println(carro.getNumPortas());
+        System.out.println("A cor do carro é: " +carro.getCor());
+        System.out.println("A placa do carro é: " +carro.getPlaca());
+        System.out.println("O carro é do tipo: " +carro.getTipo());
+        System.out.println("Numero de portas: " +carro.getNumPortas());
+        System.out.println("O carro possúi de combustível: "+carro.getCombustivel());
+        System.out.println("A marca do carro é: " +carro.getMarca());
+        System.out.println("O carro pesa: " +carro.getPeso()+"kg");
+        System.out.println("O carro foi fabricado em: " +carro.getAnoFabricacao());
     }
 }
