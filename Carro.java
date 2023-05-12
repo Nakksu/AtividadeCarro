439ca154b67bd9edd444378a8c320108ed40911f
Emily Carolina <128648356+Huoyiin@users.noreply.github.com>
2023-05-12 20:53:17 -0300

    Carro.java

--- Carro.java
+++ Carro.java
@@ -1,5 +1,41 @@
 public class Carro {
 
+    double combustivel;
+    double peso;
+    String marca;
+    int anoFabricacao;
+
+    void setCombustivel(double combustivel) {
+        this.combustivel = combustivel;
+    }
+
+    double getCombustivel() {
+        return this.combustivel;
+    }
+
+    void setPeso(double peso) {
+        this.peso = peso;
+    }
+
+    double getPeso() {
+        return this.peso;
+    }
+
+    void setMarca(String marca) {
+        this.marca = marca;
+    }
+
+    String getMarca() {
+        return this.marca;
+    }
+
+    void setAnoFabricacao(int anoFabricacao) {
+        this.anoFabricacao = anoFabricacao;
+    }
+
+    int getAnoFabricacao() {
+        return this.anoFabricacao;
+    }
     String tipo;
     String cor;
     String placa;
@@ -37,3 +73,4 @@ public class Carro {
         return this.numPortas;
     }
 }
+
